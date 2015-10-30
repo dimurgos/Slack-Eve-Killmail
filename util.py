45a07622a0c0a7870da056ddd093c321bf8d3794
Dimurgos <ywn@dimurgos.com>
2015-10-30 14:44:07 +0100

    Added Accept header

--- util.py
+++ util.py
@@ -6,6 +6,7 @@ import config
 def get_public_crest_data(type, id):
     request = urllib2.Request('https://public-crest.eveonline.com/{0}/{1}/'.format(type, id))
     request.add_header('User-Agent', config.config_header)
+    request.add_header('Accept', 'application/vnd.ccp.eve.Api-v3+json;charset=utf-8')
     opener = urllib2.build_opener()
     data = opener.open(request)
     return json.load(data)
@@ -13,6 +14,7 @@ def get_public_crest_data(type, id):
 def get_public_crest_data_href(href):
     request = urllib2.Request(href)
     request.add_header('User-Agent', config.config_header)
+    request.add_header('Accept', 'application/vnd.ccp.eve.Api-v3+json;charset=utf-8')
     opener = urllib2.build_opener()
     data = opener.open(request)
     return json.load(data)
