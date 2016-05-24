977704d5d38aaec9aec9b302e164db68802e6bbf
Dimurgos <ywn@dimurgos.com>
2016-05-24 17:59:47 +0200

    Changed to the new crest URL

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import config
 import version
 
 def get_public_crest_data(type, id):
-    request = urllib2.Request('https://public-crest.eveonline.com/{0}/{1}/'.format(type, id))
+    request = urllib2.Request('https://crest-tq.eveonline.com/{0}/{1}/'.format(type, id))
     request.add_header('User-Agent', config.config_header)
     request.add_header('Accept', 'application/vnd.ccp.eve.{0}+json;charset=utf-8'.format(version.api_versions[type]))
     opener = urllib2.build_opener()
