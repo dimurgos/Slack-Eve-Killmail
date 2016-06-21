82eaca2e48f94f9fa2121aa9ee1f1691ebf172e7
Dimurgos <ywn@dimurgos.com>
2016-06-21 23:35:24 +0200

    Reverted change, added proper map to crest version

--- ships.py
+++ ships.py
@@ -6,7 +6,7 @@ def get_ship_by_id(id):
     if id in ship_list:
         return ship_list[id]
         
-    root = util.get_public_inventory_crest_data('types', id)
+    root = util.get_public_crest_data('inventory/types', id)
     
     typeName = root['name']
     ship_list[id] = typeName
--- version.py
+++ version.py
@@ -2,5 +2,5 @@ api_versions = {
 	'solarsystems': 'System-v1',
 	'regions': 'Region-v1',
 	'constellations': 'Constellation-v1',
-	'types': 'ItemType-v3'
+	'inventory/types': 'ItemType-v3'
 }
